843a627eb2be13361792738c1b893312ae2a8045
Gerasimos Alexiou <geralexgr@gmail.com>
2022-09-20 11:42:40 +0300

    change animation to 700 ms

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@
                     {
                         scrollTop: target.offset().top,
                     },
-                    500,
+                    700,
                     "easeInOutExpo"
                 );
                 return false;
